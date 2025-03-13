3044faaa7f95ccdfdf8ddd2b2bfa41f7d2f2d7c8
Ivan <Ivan@DESKTOP-19I331C>
2025-03-13 08:24:03 +0200

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Money.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryID
+    }
+}
